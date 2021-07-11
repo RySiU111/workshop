72f0cbc3a1c7842d170e1b72382ed2763ed4bd05
Michał Rychlik <RySiU111@users.noreply.github.com>
2021-07-11 17:58:49 +0200

    swagger

--- Workshop.API/Startup.cs
+++ Workshop.API/Startup.cs
@@ -69,9 +69,11 @@ namespace Workshop.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Workshop.API v1"));
+                
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Workshop.API v1"));
 
             app.UseHttpsRedirection();
 
