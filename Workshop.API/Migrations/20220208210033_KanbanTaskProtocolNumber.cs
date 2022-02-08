0299a5fc57ff4bd364205f276ab7daa36763cb78
Michał Rychlik <RySiU111@users.noreply.github.com>
2022-02-08 22:46:44 +0100

    KanbbanTask Protocol

---
+++ Workshop.API/Migrations/20220208210033_KanbanTaskProtocolNumber.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Workshop.API.Migrations
+{
+    public partial class KanbanTaskProtocolNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProtocolNumber",
+                table: "KanbanTasks",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProtocolNumber",
+                table: "KanbanTasks");
+        }
+    }
+}
