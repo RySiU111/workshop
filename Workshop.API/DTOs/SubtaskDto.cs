81195686762c0fd6c7ce639402ac7c7a017acbef
Michał Rychlik <RySiU111@users.noreply.github.com>
2022-02-08 20:04:22 +0100

    HotFix

--- Workshop.API/DTOs/SubtaskDto.cs
+++ Workshop.API/DTOs/SubtaskDto.cs
@@ -6,7 +6,7 @@ namespace Workshop.API.DTOs
     {
         public int Id { get; set; }
         public int KanbanTaskId { get; set; }
-        public int UserId { get; set; }
+        public int? UserId { get; set; } = null;
         public string Name { get; set; }
         public double ManHour { get; set; }
         public SubtaskStatus Status { get; set; }
